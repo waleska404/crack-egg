b851ca45ac4b0eac1ad1f3926ee44b13d71db924
Paula Boyano Ivars <paula.boyano@vass.es>
2023-12-24 18:07:13 +0100

    Initial Commit

---
+++ CrackEgg/CrackEggApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrackEggApp.swift
+//  CrackEgg
+//
+//  Created by Paula Boyano Ivars on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CrackEggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
